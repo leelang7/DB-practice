9d3430ab46f788f23d5c349df1b6917cc1ed0a1b
Fixer <67309088+leelang7@users.noreply.github.com>
2025-04-04 16:40:18 +0900

    update

--- DB3/set_sql/q17_set.sql
+++ DB3/set_sql/q17_set.sql
@@ -1,17 +1,16 @@
-CREATE TABLE EMPLOYEE(
-  employee_id	INT NOT NULL PRIMARY KEY,
-  salary		INT NOT NULL,
-  position		VARCHAR(10) NOT NULL
+CREATE TABLE FOOD(
+  food_id	INT 			NOT NULL PRIMARY KEY,
+  food_type	VARCHAR(30)		NOT NULL,
+  food_name	VARCHAR(100)	NOT NULL,
+  price		INT
 );
 
-INSERT INTO EMPLOYEE VALUES
-(10000, 9000, '사장'),
-(10001, 8000, '부장'),
-(10002, 7800, '과장'),
-(10003, 7500, '과장'),
-(10004, 6800, '대리'),
-(10005, 7000, '대리'),
-(10006, 6000, '대리'),
-(10007, 5500, '사원'),
-(10008, 4500, '사원'),
-(10009, 7000, '사원');
+INSERT INTO FOOD VALUES
+(1, 'Appetizer', 'Salad', 5000),
+(2, 'Appetizer', 'Soup', 3500),
+(3, 'Main', 'Pasta', 8000),
+(4, 'Main', 'Steak', 11000),
+(5, 'Main', 'rice', 7000),
+(6, 'Dessert', 'Ice-cream', 10000),
+(7, 'Dessert', 'Cookie', 5000),
+(8, 'Dessert', 'Coffee', 8000);
