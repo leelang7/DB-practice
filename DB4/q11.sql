cbef4708d2bd8344ec4c5cf82321e59bb7c77282
AI Diver <67309088+leelang7@users.noreply.github.com>
2025-04-14 11:53:20 +0900

    first commit

---
+++ DB4/q11.sql
@@ -0,0 +1,9 @@
+-- 아래는 PRODUCT, SELL 테이블에 대한 구조를 보여줍니다. 테이블을 수정하실 필요는 없습니다.
+DESC PRODUCT;
+DESC SELL;
+
+-- 1. SELL 테이블과 PRODUCT 테이블을 상품 ID 기준으로 INNER JOIN 하는 쿼리를 작성하세요.
+
+
+-- 2. SUM 집계 함수를 이용하여, 판매자 이름, 상품 ID 별 판매 금액을 구하는 쿼리를 작성하세요.
+
---
+++ DB4/q4.sql
@@ -0,0 +1,6 @@
+-- 아래 쿼리는 MEMBER 테이블의 구조를 나타냅니다. 해당 테이블을 수정할 필요는 없습니다.
+DESC MEMBER;
+
+-- 계층형 질의, WITH RECURSIVE 를 이용하여, member_id, manager_id, lvl을 출력하되
+-- member_id 오름차순, lvl 오름차순 정렬을 적용하여 쿼리를 작성하세요.
+
