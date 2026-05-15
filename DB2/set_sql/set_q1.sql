b438bc9f1bd070e6f86ce57af3fa5229a52c5faa
All that AI <67309088+leelang7@users.noreply.github.com>
2026-05-15 09:45:32 +0900

    update

--- DB2/set_sql/set_q1.sql
+++ DB2/set_sql/set_q1.sql
@@ -1,24 +1,18 @@
-CREATE TABLE employees (
-    emp_no      INT             NOT NULL,
-    birth_date  DATE            NOT NULL,
-    first_name  VARCHAR(14)     NOT NULL,
-    last_name   VARCHAR(16)     NOT NULL,
-    gender      ENUM ('M','F')  NOT NULL,    
-    hire_date   DATE            NOT NULL,
-    superior_no INT                     ,
-    PRIMARY KEY (emp_no)
+create table if not exists book(
+    id int not null,
+    title varchar(64) not null,
+    author varchar(64),
+    publisher varchar(32),
+    date_received date,
+    primary key (id)
 );
 
-ALTER TABLE employees ADD FOREIGN KEY (superior_no) REFERENCES employees(emp_no);
-
-INSERT INTO employees VALUES
-(10001,'1953-09-02','Georgi','Facello','M','1986-06-26', NULL),
-(10002,'1964-06-02','Bezalel','Simmel','F','1985-11-21', NULL),
-(10003,'1959-12-03','Parto','Bamford','M','1986-08-28', NULL),
-(10004,'1954-05-01','Chirstian','Koblick','M','1986-12-01', NULL),
-(10005,'1955-01-21','Kyoichi','Maliniak','M','1989-09-12', 10001),
-(10006,'1953-04-20','Anneke','Preusig','F','1989-06-02', 10001),
-(10007,'1957-05-23','Tzvetan','Zielinski','F','1989-02-10', 10002),
-(10008,'1958-02-19','Saniya','Kalloufi','M','1994-09-15', 10002),
-(10009,'1952-04-19','Sumant','Peac','F','1985-02-18', NULL),
-(10010,'1963-06-01','Duangkaew','Piveteau','F','1989-08-24', 10001);
+insert into book values(1000, 'Romeo and Juliet', 'William Shakespeare', null, '2012-04-01');
+insert into book values(1001, 'Don Quixote', 'Miguel de Cervantes Saavedra', null, '2015-03-31');
+insert into book values(1002, 'The Little Prince', 'Antoine de Saint-Exupery', null, '2018-12-21');
+insert into book values(1003, 'Harry Potter and the Philosopher\'s Stone', 'Joanne Kathleen Rowling', 'Bloomsbury Publishing', '2017-06-26');
+insert into book values(1004, 'The Lord of the Rings', 'John Ronald Reuel Tolkien', 'Allen & Unwin', '2014-07-29');
+insert into book values(1006, 'Alice\'s Adventures in Wonderland', 'Lewis Carroll', 'Macmillan ', '2015-11-26');
+insert into book values(1007, 'The Old Man and the Sea', 'Ernest Miller Hemingway', 'Charles Scribner\'s Sons', '2014-11-02');
+insert into book values(1008, 'The Great Gatsby', 'Francis Scott Key Fitzgerald', null, '2019-01-12');
+insert into book values(1009, 'Harry Potter and the Deathly Hallows', 'Joanne Kathleen Rowling', 'Bloomsbury Publishing', '2017-07-21');
